fed0216e4c6d554e23bd1bf15fdc28d6e039af7f
gcca <git@gcca.dev>
2025-11-15 10:35:41 -0500

    Scripts

---
+++ fish/functions/cp-signin.fish
@@ -0,0 +1,12 @@
+function cp-signin -d "Sign in to Control Plane"
+  argparse 's/staging' 'p/prod' -- $argv
+  if not command -q http
+    source ~/Developer/e/13/bin/activate.fish
+  end
+  if set -q _flag_p
+    set -g CP_URL $CP_BASE_URL_PROD
+  else
+    set -g CP_URL $CP_BASE_URL_STAGING
+  end
+  set -g CP_TOKEN (http --verify=no --form $CP_URL/api/auth/token username=$CP_USERNAME password=$CP_PASSWORD | python -c "import json,sys;print(json.load(sys.stdin)['access_token'])")
+end
---
+++ fish/functions/git-diff-4llm.fish
@@ -0,0 +1,3 @@
+function git-diff-4llm
+  for file in (git diff --name-only --cached); echo "=== $file (old) ==="; git show HEAD:"$file"; echo "=== $file (new) ==="; cat "$file"; echo "---"; end
+end
