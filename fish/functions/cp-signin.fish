93a647b404544f0e3d3e709ec241b43662ffb988
gcca <git@gcca.dev>
2026-02-04 08:48:59 -0500

    20260204084859

--- fish/functions/cp-signin.fish
+++ fish/functions/cp-signin.fish
@@ -4,9 +4,9 @@ function cp-signin -d "Sign in to Control Plane"
         source ~/Developer/e/13/bin/activate.fish
     end
     if set -q _flag_s
-        set CP_URL $CP_BASE_URL_STAGING
+        set -g CP_URL $CP_BASE_URL_STAGING
     else
-        set CP_URL $CP_BASE_URL_PROD
+        set -g CP_URL $CP_BASE_URL_PROD
     end
-    set CP_TOKEN (http --verify=no --form $CP_URL/api/auth/token username=$CP_USERNAME password=$CP_PASSWORD | python -c "import json,sys;print(json.load(sys.stdin)['access_token'])")
+    set -g CP_TOKEN (http --verify=no --form $CP_URL/api/auth/token username=$CP_USERNAME password=$CP_PASSWORD | python -c "import json,sys;print(json.load(sys.stdin)['access_token'])")
 end
