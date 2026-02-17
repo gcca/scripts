b896bb0b5859502dbd2d73b18b27c19c4880c0a5
gcca <git@gcca.dev>
2026-02-17 08:26:08 -0500

    20260217082608

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     #: {{{ gcca
     if test (uname) = Darwin
-        if test -d ~/.hb
+        if test -x ~/.hb/bin/brew
             eval (~/.hb/bin/brew shellenv)
         else
             eval (/opt/homebrew/bin/brew shellenv)
@@ -33,6 +33,8 @@ if status is-interactive
     set -x LANG en_US.UTF-8
     set -x LC_ALL en_US.UTF-8
     set -x LC_CTYPE en_US.UTF-8
+
+    alias tailscale=/Applications/Tailscale.app/Contents/MacOS/Tailscale
 
     set -x NODE_OPTIONS "--localstorage-file=$HOME/.coc-localstorage.db"
 
